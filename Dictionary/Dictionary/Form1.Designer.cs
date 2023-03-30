15d4e29a122073adaea0fe224fd593463becc283
Anastas Acevski <123334501+Ace9000@users.noreply.github.com>
2023-03-31 00:01:53 +0200

    Add files via upload

---
+++ Dictionary/Dictionary/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ValueListBox = new System.Windows.Forms.ListBox();
+            this.keyListBox = new System.Windows.Forms.ListBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
+            this.txtKeyy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ValueListBox
+            // 
+            this.ValueListBox.FormattingEnabled = true;
+            this.ValueListBox.ItemHeight = 16;
+            this.ValueListBox.Location = new System.Drawing.Point(240, 311);
+            this.ValueListBox.Name = "ValueListBox";
+            this.ValueListBox.Size = new System.Drawing.Size(187, 260);
+            this.ValueListBox.TabIndex = 35;
+            // 
+            // keyListBox
+            // 
+            this.keyListBox.FormattingEnabled = true;
+            this.keyListBox.ItemHeight = 16;
+            this.keyListBox.Location = new System.Drawing.Point(26, 311);
+            this.keyListBox.Name = "keyListBox";
+            this.keyListBox.Size = new System.Drawing.Size(187, 260);
+            this.keyListBox.TabIndex = 34;
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(273, 247);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(136, 35);
+            this.button10.TabIndex = 33;
+            this.button10.Text = "Find the Value";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // txtValue
+            // 
+            this.txtValue.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValue.Location = new System.Drawing.Point(94, 254);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(169, 24);
+            this.txtValue.TabIndex = 32;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(7, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 31;
+            this.label3.Text = "Find Value:";
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(273, 193);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(136, 35);
+            this.button9.TabIndex = 30;
+            this.button9.Text = "Find the Key";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // txtKeyy
+            // 
+            this.txtKeyy.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKeyy.Location = new System.Drawing.Point(95, 199);
+            this.txtKeyy.Name = "txtKeyy";
+            this.txtKeyy.Size = new System.Drawing.Size(169, 24);
+            this.txtKeyy.TabIndex = 29;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(21, 201);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Find Key:";
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(309, 132);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 37);
+            this.button8.TabIndex = 27;
+            this.button8.Text = "Copy To";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(222, 132);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 37);
+            this.button7.TabIndex = 26;
+            this.button7.Text = "Clear";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(24, 132);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(185, 37);
+            this.button6.TabIndex = 25;
+            this.button6.Text = "For Each by Dictionary";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(373, 67);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 29);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "Get Size";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(296, 67);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(71, 29);
+            this.btnDelete.TabIndex = 23;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWrite.Location = new System.Drawing.Point(219, 66);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(71, 29);
+            this.btnWrite.TabIndex = 22;
+            this.btnWrite.Text = "Write";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRead.Location = new System.Drawing.Point(142, 67);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(71, 29);
+            this.btnRead.TabIndex = 21;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 18);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Key";
+            // 
+            // txtKey
+            // 
+            this.txtKey.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtKey.Location = new System.Drawing.Point(49, 70);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(85, 24);
+            this.txtKey.TabIndex = 19;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(209, 38);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Add Items by Dictionary";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(227, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(209, 38);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "Sorted Dictionary";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 579);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ValueListBox);
+            this.Controls.Add(this.keyListBox);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.txtKeyy);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnWrite);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ValueListBox;
+        private System.Windows.Forms.ListBox keyListBox;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox txtKeyy;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Dictionary/Dictionary/Form1.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+            di.Add(42, "Brod");
+
+            Dictionary<string, string> di2 = new Dictionary<string, string>();
+
+            di2.Add("UK", "London");
+
+            Dictionary<string, Button> di3 = new Dictionary<string, Button>();
+
+            di3.Add("Button1",btnRead);
+
+        }
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+                di.Add(43, "Maj");
+                di.Add(54, "Telefon");
+                di.Add(62, "Avgust");
+                di.Add(89, "Tocak");
+                di.Add(74, "Motor");
+                di.Add(67, "Avion");
+                di.Add(42, "Brod");
+
+                Int32 key;
+                Int32.TryParse(txtKey.Text, out key);
+
+                Text = di[key];
+            }
+            catch
+            {
+                MessageBox.Show("Not Found");
+            }              
+            
+        }
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+            di.Add(42, "Brod");
+
+            Int32 key;
+            Int32.TryParse(txtKey.Text, out key);
+
+            di[key] = "Test";
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+            di.Add(42, "Brod");
+
+            Int32 key;
+            Int32.TryParse(txtKey.Text, out key);
+
+            Boolean r;
+            r = di.Remove(key);
+
+            if (r==true)
+            {
+                MessageBox.Show("Deleted");
+            }
+            else
+            {
+                MessageBox.Show("Noo Deleted");
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+            di.Add(42, "Brod");
+
+            txtKey.Text = di.Count().ToString();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+            di.Add(42, "Brod");
+
+            foreach (var item in di)
+            {
+                keyListBox.Items.Add(item.Key);
+                ValueListBox.Items.Add(item.Value);
+            }
+            //------------------------------------------
+            keyListBox.Items.Clear();
+            ValueListBox.Items.Clear();
+
+            foreach (KeyValuePair <Int32, string> item in di)
+            {
+                keyListBox.Items.Add(item.Key);
+                ValueListBox.Items.Add(item.Value);
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+            di.Add(42, "Brod");
+
+            di.Clear();
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+            di.Add(42, "Brod");
+
+            Int32[] target_keys = new Int32[di.Count()];
+            di.Keys.CopyTo(target_keys, 0);
+
+            //-------------------------------------------------------
+            string[] target_values = new string[di.Count()];
+            di.Values.CopyTo(target_values, 0);
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+
+            Int32 key;
+            Int32.TryParse(txtKeyy.Text, out key);
+            Boolean r;
+            r = di.ContainsKey(key);
+
+            if (r == true)
+            {
+                MessageBox.Show("Fond");
+            }
+            else
+            {
+                MessageBox.Show("Not found");
+            }
+
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+
+            Dictionary<Int32, string> di = new Dictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+
+            //string key;
+            //string.TryParse(txtValue.Text, out key);
+            Boolean r;
+            r = di.ContainsValue(txtValue.Text);
+
+            if (r == true)
+            {
+                MessageBox.Show("Fond");
+            }
+            else
+            {
+                MessageBox.Show("Not found");
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            SortedDictionary<Int32, string> di = new SortedDictionary<Int32, string>();
+
+            di.Add(43, "Maj");
+            di.Add(54, "Telefon");
+            di.Add(62, "Avgust");
+            di.Add(89, "Tocak");
+            di.Add(74, "Motor");
+            di.Add(67, "Avion");
+        }
+    }
+}
